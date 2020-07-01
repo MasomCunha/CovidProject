034460719c5d0330fbf45bdd679168e106b873d4
Manuel <Manuelasomc@gmail.com>
2020-07-01 16:35:54 +0100

    default route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CovidApp from './pages/CovidApp.jsx'
 import SingleCountry from './pages/SingleCountry.jsx'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 
 function App() {
@@ -9,7 +9,8 @@ function App() {
     <div className="App" style = {{ "background-color" : "#a3b6ad"}}>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact={true} component={CovidApp} />
+        {/* <Redirect from="/" to="/CovidProject"/> */}
+          <Route path={["/","/CovidProject"]} exact={true} component={CovidApp} />
           <Route path="/country/:country" component={SingleCountry} />
         </Switch>
       </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
+serviceWorker.unregister();
