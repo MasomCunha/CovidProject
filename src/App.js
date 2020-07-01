7088342c308de7d38feeb87559aaac8853bba386
Manuel <Manuelasomc@gmail.com>
2020-07-01 16:43:08 +0100

    solving heroku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CovidApp from './pages/CovidApp.jsx'
 import SingleCountry from './pages/SingleCountry.jsx'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 
 function App() {
@@ -9,7 +9,6 @@ function App() {
     <div className="App" style = {{ "background-color" : "#a3b6ad"}}>
       <BrowserRouter>
         <Switch>
-        {/* <Redirect from="/" to="/CovidProject"/> */}
           <Route path={["/","/CovidProject"]} exact={true} component={CovidApp} />
           <Route path="/country/:country" component={SingleCountry} />
         </Switch>
